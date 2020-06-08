0ad958d01cd11eaa110c5120f29028d0e5895fc5
Chris Nolin <cnolin@ladmins-MacBook-Pro-2.local>
2020-06-08 13:34:25 -0600

    first commit

--- server.js
+++ server.js
@@ -11,15 +11,36 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-/* Middleware*/
+// master fundamentals:
+//   server: something that give and receive instructions
+//   process: instatiation of set of instructions with memory, executing on cpu; fundamental unit of computation on a computer; particular instance of acting on set of instructions
+//   every chrome tab is a process, as is the terminal
+//   programs built up from one or more processes
+//   server: special type of process, accepting requests and serving responses; long running process that you can communicate with and it'll do something
+//   localhost is not a server; it's a domain name that points to 127.0.0.1 (loopback address), an ip address, local address of computer on computer's local network
+//   every process hooked onto internet has an ip address
+//   an interace has only one ip address; might talk to multiple processes, that all need access to internet
+//   wifi card, router has its own ip address
+//   every process gets a unique port so it can access internet
+//   express app requests port 3000, so it's on localhost:3000, so that's how we can talk to it
+//   can have multiple express apps with different ports like 3000 or 3001, up to 65k ports per machine
+//   I was running on another port, not port 3000, so not running on our server; probably running on a built-in mac server, which don't have control over; better to easily make server on, perhaps, node.js
+//   master html, css, js for web development
+//      know high level concepts and subconcepts
+//      know practical knowledge, so you're implementing what concepts you're learning
+//      general 
+
+ 
+/* Middleware*/ //when a specific path is called, middleware works
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
+//handler is a function that handles an event; in Express, you have request handlers, similar to middleware
 // Cors for cross origin allowance
+app.use(cors()); // will return a middleware function; can only make request to your origin
 
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static('website')); //middleware for get? // https://expressjs.com/en/starter/static-files.html
 
 
 // Setup Server
@@ -29,28 +50,29 @@ const port = 3000;
 const server = app.listen(port, listening);
 
 function listening() {
-    console.log("server is running");
     console.log(`server is running on port: ${port}`);
 }
 
-app.get('/weather-journal-app', function(req, res) {
+
+app.get('/all', check)
+
+function check(req, res) {
     res.send(projectData);
-});
+}
 
-console.log("testing...");
-
-app.post('/post', addInfo);
 
 
 function addInfo(req, res) {
-    console.log("in addInfo");
     let ndata = req.body;
-    let nEntry = {
-        temperature: temperature,//ndata.temp,
-        date: date,//ndata._date,
-        user_resp: user_resp//ndata.u_resp
-    }
-    projectData.push(nEntry);
-    res.send(projectData);
-    console.log(projectData);
+    projectData.temperature = ndata.temperature;
+    projectData.date = ndata.date;
+    projectData.user_resp = ndata.user_resp;
+    //serialization: turn json into a string
+    res.send(JSON.stringify(projectData));
 }
+
+app.post('/weather', addInfo)
+
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!")
+})
--- website/app.js
+++ website/app.js
@@ -1,22 +1,21 @@
 /* Global Variables */
 
-let api_key = '&appid=08ca1ff33ce4bb1e3f625332f2411fec';
-let base_url = 'https://api.openweathermap.org/data/2.5/weather?zip='; 
+const api_key = '&appid=08ca1ff33ce4bb1e3f625332f2411fec';
+let base_url = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+let imp_units = '&units=imperial';
 
 document.getElementById('generate').addEventListener('click', p_action);
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let m = d.getMonth() +1
+let newDate = m+'/'+ d.getDate()+'/'+ d.getFullYear();
                               
 // fetch GET request to api
 const getWeather = async (base_url, zipc, api_key)=>{
-    const res = await fetch(base_url+zipc+api_key)
+    const res = await fetch(base_url+zipc+api_key+imp_units)
     try{
         const data = await res.json();
-        console.log("data.main.temp in getWeather: "+data.main.temp);
-        console.log("data.name in getWeather: "+data.name);
-        console.log("get appears to work fine");
         return data;        
     } catch(error) {
         console.log("Error in fetch GET", error);
@@ -26,7 +25,6 @@ const getWeather = async (base_url, zipc, api_key)=>{
 
 // fetch POST
 const postData = async(url='', data)=>{
-    console.log("postData url: "+url);
     const response = await fetch(url, {
         method: 'POST',
         credentials: 'same-origin',
@@ -36,11 +34,7 @@ const postData = async(url='', data)=>{
         body: JSON.stringify(data),
     });
     try{
-        const newData = await response.text(); //response.text() shows that it just contains html
-        console.log("postData newData: "+newData)
-        return newData;
-        //let newData = {"temperature": "55", "date": "4.14.2020", "user_resp": "g"};
-        //console.log(`postData try newData: ${newData}; newData.temperature: ${newData.temperature}`);
+        const newData = await response.json();
         return newData;
     }catch(error){
         console.log(`Try error: ${error}`);
@@ -48,18 +42,13 @@ const postData = async(url='', data)=>{
 }
 
 
-const updatePage = async (url='', data) => {
-    //console.log("Then, now into updatePage");
-  const request = await fetch('http://localhost/weather-journal-app/website');
+const updatePage = async () => {
+  const request = await fetch('/all')
   try{
-    const allData = await request.text();
-      //console.log("allData: "+allData);
-      console.log("allData.temperature: "+allData.temperature);
-      console.log("allData[0]: "+allData[0]);
-      console.log("allData[0].date: "+allData[0].date);
-    document.getElementById('date').innerHTML = "Hi!";//allData[0].date;
-    document.getElementById('temp').innerHTML = allData[0].temperature;
-    document.getElementById('content').innerHTML = allData[0].user_resp;
+    const allData = await request.json();
+    document.getElementById('date').innerHTML = "The date today is: " +allData.date;
+    document.getElementById('temp').innerHTML = `Today's temperature is: ${allData.temperature}F`;
+    document.getElementById('content').innerHTML = `Your feelings: ${allData.user_resp}`;
 
   }catch(error){
     console.log("error", error);
@@ -73,9 +62,8 @@ function p_action(e){
     getWeather(base_url, zipc, api_key)
     .then(function(data){
         const u_resp = document.getElementById('feelings').value;
-        console.log(`p_action's data has temperature: ${data.main.temp}; date: ${newDate}; user_resp: ${u_resp}`)
-        postData('http://localhost/weather-journal-app/', {temperature: 44, date: "4.15.2020", user_resp: "g"}); // {temperature: data.main.temp, date: newDate, user_resp: u_resp});
-    }).then(
-        updatePage()
-    );
+        postData('/weather', {temperature: data.main.temp, date: newDate, user_resp: u_resp});
+    }).then(function(data){
+        updatePage('/all');
+    })
 }
