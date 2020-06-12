d64bb585748d33707543d782efe73762394a97d5
Chris Nolin <cnolin@ladmins-MacBook-Pro-2.local>
2020-06-12 16:58:07 -0600

    set updatePage to fill in forecast

--- src/client/js/updatePage.js
+++ src/client/js/updatePage.js
@@ -2,10 +2,8 @@ const updatePage = async () => {
   const request = await fetch('/all')
   try{
     const allData = await request.json();
-      console.log("updatePage's allData: ",allData);
-    
-    /*document.getElementById('temp').innerHTML = `Your city's longitude is: ${allData.lng}`;
-    document.getElementById('content').innerHTML = `Your city's latitude is: ${allData.lat}`; */
+    document.getElementById('forecast').innerHTML = 
+        `<h2>The ${allData.date} forecast for ${allData.city_name} is cloud coverage of ${allData.clouds}% of the sky, a high of ${allData.high_temp}F with a low of ${allData.low_temp}F, and ${allData.precip}mm of rain.</h2>`;
 
   }catch(error){
     console.log("error", error);
